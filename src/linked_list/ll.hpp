5f187a4183d9f935158a1a8dddc499fdfd86c8ee
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2017-05-15 23:54:18 +0800

    add some more code for LL

--- src/linked_list/ll.hpp
+++ src/linked_list/ll.hpp
@@ -7,6 +7,7 @@ struct ll_node{
 	ll_node *next;
 };
 
+// assign value to node and return
 template<class T>
 ll_node<T> *create_node(T new_data){
 	ll_node<T> *new_node = NULL;
@@ -16,23 +17,50 @@ ll_node<T> *create_node(T new_data){
 	return new_node;
 }
 
+// print list
 template<class T>
 void print_list(struct ll_node<T> *head){
 	while(head){
-		cout << head->data << endl;
+		cout << head->data << " ";
 		head = head->next;
 	}
+	cout << endl;
 }
 
+// get length of list
 template<class T>
 int get_length(struct ll_node<T> *head){
 	int length = 0;
+	cout << "List:\n";
 	while(head){
 		length++;
 		cout << head->data << " ";
 		head = head->next;
 	}
+	cout << endl;
 	return length;
 }
 
+// convert an array to a linked list
+template<class T>
+ll_node<T> *create_list_from_array(int length, T arr[]){
+	if(length == 0) return NULL;
+	ll_node<T> *head = create_node(arr[0]);
+	ll_node<T> *temp = head;
 
+	for(int i=1;i<length;i++){
+		temp->next = create_node(arr[i]);
+		temp = temp->next;
+	}
+
+	return head;
+}
+
+// push at the starting of a linked list
+template<class T>
+void push(ll_node<T> **head_ref, T data){
+	ll_node<T> *new_node = create_node(data);
+	new_node->data = data;
+	new_node->next = *head_ref;
+	*head_ref = new_node;
+}
--- test/linked_list/test_ll.cpp
+++ test/linked_list/test_ll.cpp
@@ -3,7 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-TEST(stlmp, linked_list){
+TEST(stlmp, linked_list_simple){
 	ll_node<int> *head = create_node(1);
 	head->next = create_node(2);
 	cout << "starting to print list" << endl;
@@ -12,4 +12,17 @@ TEST(stlmp, linked_list){
 
 	cout << "Checking the length of the list" << endl;
 	ASSERT_EQ(get_length(head), 2);
+}
+
+TEST(stlmp, linked_list_from_array){
+	int foo[] = {1, 2, 3, 4, 5};
+	ll_node<int> *head = create_list_from_array(sizeof(foo)/sizeof(foo[0]), foo);
+	ASSERT_EQ(get_length(head), sizeof(foo)/sizeof(foo[0]));
+}
+
+TEST(stlmp, linked_list_push){
+	int foo[] = {1, 2, 3, 4, 5};
+	ll_node<int> *head = create_list_from_array(sizeof(foo)/sizeof(foo[0]), foo);
+	push(&head, 6);
+	ASSERT_EQ(get_length(head), 1 + sizeof(foo)/sizeof(foo[0]));
 }
