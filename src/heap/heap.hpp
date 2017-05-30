590a07ade095ec0a81b100b7f2e3a4609148dc82
Man Parvesh Singh Randhawa <randhawa_m@worksap.co.jp>
2017-05-30 18:56:49 +0800

    Add down heapify, add to CMake file
    TODO: write tests, other functions for heap

--- src/heap/heap.hpp
+++ src/heap/heap.hpp
@@ -13,9 +13,9 @@ public:
 	heap(int capacity, int type) : arr(new T[capacity]), size(0), capacity(capacity), type(type) {}
 
 	// get private values
-	int size(){ return this->size; }
-	int capacity(){ return this->capacity; }
-	int type(){ return this->type; }
+	int get_size(){ return this->size; }
+	int get_capacity(){ return this->capacity; }
+	int get_type(){ return this->type; }
 
 	// util functions
 	// get index of parent from child index
@@ -52,5 +52,26 @@ public:
 		return this->arr[0];
 	}
 
+	// takes location of element as input and heapifies our array downwards
+	// considering max heap
+	void down(int parent_index){
+		int left_child_index = get_left_child(), right_child_index = get_right_child();
+		if(left_child_index == -1 && right_child_index == -1) return;
 
+		// everything is valid, so let's proceed to finding the next max element that can be swapped with the parent
+		int max_element;
+		if(left_child_index != -1 && arr[left_child_index] > arr[parent_index]) max_element = left_child_index;
+		else max_element = parent_index;
+
+		if(right_child_index != -1 && arr[right_child_index] > arr[max_element]) max_element = right_child_index;
+
+		// swap max_element with parent
+		if(max_element != parent_index){
+			int temp = arr[max_element];
+			arr[max_element] = arr[parent_index];
+			arr[parent_index] = temp;
+		}
+
+		down(max_element);
+	}
 };
--- test/heap/test_heap.cpp
+++ test/heap/test_heap.cpp
@@ -5,5 +5,5 @@
 using namespace std;
 
 TEST(heap, all_tests){
-	
+	heap<int> *h = new heap<int>(10, 1);
 }
