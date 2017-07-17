c5f9e3d81f9b19dc2da5a1e87d12687540384d93
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2017-07-17 14:11:46 +0800

    update build scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,34 @@
-mkdir build
+# define colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+# create build directory
+if [ -d "build" ]; then
+	echo 'directory build exists, so not creating again'
+else
+	mkdir build
+fi
+
+echo 'cd to build directory'
 cd build
+
+echo 'cleaning build dir'
 rm -r *
+
+echo 'running cmake'
 cmake ..
-make
-./unit_tests
+
+if [ $? -eq 0 ]; then
+    printf "${green}cmake succeeded${reset}\n"
+	echo 'running make'
+	make
+	if [ $? -eq 0 ]; then
+	    printf "${green}make succeeded, running tests${reset}\n"
+	    ./unit_tests
+	else
+	    printf "${red}make failed${reset}\n"
+	fi
+else
+    printf "${red}cmake failed${reset}\n"
+fi
--- build_after_cmake.sh
+++ build_after_cmake.sh
@@ -1,3 +1,19 @@
-cd build
-make
-./unit_tests
+# define colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+if [ -d "build" ]; then
+	echo 'directory build exists'
+	cd build
+	echo 'running make'
+	make
+	if [ $? -eq 0 ]; then
+	    printf "${green}make succeeded, running tests${reset}\n"
+	    ./unit_tests
+	else
+	    printf "${red}make failed${reset}\n"
+	fi
+else
+	printf "${red}build directory doesn't wxist. Please execute build.sh file instead of this one${reset}\n"
+fi
