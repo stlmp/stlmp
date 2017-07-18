107f95ca8b593c6042a1700e1d44881856c2c2e7
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2017-07-18 12:51:18 +0800

    unweighted shortest path algorithm

--- test/graph/test_adjacency_list.cpp
+++ test/graph/test_adjacency_list.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "adjacency_list.hpp"
 #include "topological_sort.hpp"
+#include "shortest_paths.hpp"
 #include <iostream>
 #include <vector>
 
@@ -54,3 +55,97 @@ TEST(graph_adjacency_list, topological_sort){
 	sorted_graph.pop();
 }
 
+TEST(graph_adjacency_list, unweighted_shortest_path){
+	int vertices[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	vector< vector<int> > graph((int)(sizeof(vertices) / sizeof(vertices[0])));
+	graph[0].push_back(1);
+	graph[1].push_back(2);
+	graph[2].push_back(3);
+	graph[3].push_back(4);
+	graph[4].push_back(5);
+	graph[5].push_back(6);
+	graph[6].push_back(7);
+	graph[7].push_back(8);
+
+	pair< vector<int>, vector<int> > pr = unweighted_shortest_path(graph, 0);
+	std::vector<int> distance = pr.first, path = pr.second;
+	ASSERT_EQ(distance.size(), path.size());
+	cout << "vertex | distance | path (prev node) " << endl;
+	for(int i = 0; i < distance.size(); i++){
+		cout << "   " << i << "        " << distance[i] << "        " << path[i] << endl;
+		EXPECT_EQ(i, distance[i]);
+		if(i != 0) EXPECT_EQ(i, path[i] + 1);
+	}
+}
+
+// TODO: all below
+/*
+TEST(graph_adjacency_list, weighted_shortest_path_dijkstra){
+	int vertices[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	vector< vector<int> > graph((int)(sizeof(vertices) / sizeof(vertices[0])));
+	graph[0].push_back(1);
+	graph[1].push_back(2);
+	graph[2].push_back(3);
+	graph[3].push_back(4);
+	graph[4].push_back(5);
+	graph[5].push_back(6);
+	graph[6].push_back(7);
+	graph[7].push_back(8);
+
+	pair< vector<int>, vector<int> > pr = unweighted_shortest_path(graph, 0);
+	std::vector<int> distance = pr.first, path = pr.second;
+	ASSERT_EQ(distance.size(), path.size());
+	cout << "vertex | distance | path (prev node) " << endl;
+	for(int i = 0; i < distance.size(); i++){
+		cout << "   " << i << "        " << distance[i] << "        " << path[i] << endl;
+		EXPECT_EQ(i, distance[i]);
+		if(i != 0) EXPECT_EQ(i, path[i] + 1);
+	}
+}
+
+TEST(graph_adjacency_list, weighted_shortest_path_bellman_ford){
+	int vertices[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	vector< vector<int> > graph((int)(sizeof(vertices) / sizeof(vertices[0])));
+	graph[0].push_back(1);
+	graph[1].push_back(2);
+	graph[2].push_back(3);
+	graph[3].push_back(4);
+	graph[4].push_back(5);
+	graph[5].push_back(6);
+	graph[6].push_back(7);
+	graph[7].push_back(8);
+
+	pair< vector<int>, vector<int> > pr = unweighted_shortest_path(graph, 0);
+	std::vector<int> distance = pr.first, path = pr.second;
+	ASSERT_EQ(distance.size(), path.size());
+	cout << "vertex | distance | path (prev node) " << endl;
+	for(int i = 0; i < distance.size(); i++){
+		cout << "   " << i << "        " << distance[i] << "        " << path[i] << endl;
+		EXPECT_EQ(i, distance[i]);
+		if(i != 0) EXPECT_EQ(i, path[i] + 1);
+	}
+}
+
+TEST(graph_adjacency_list, weighted_shortest_path_prims){
+	int vertices[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	vector< vector<int> > graph((int)(sizeof(vertices) / sizeof(vertices[0])));
+	graph[0].push_back(1);
+	graph[1].push_back(2);
+	graph[2].push_back(3);
+	graph[3].push_back(4);
+	graph[4].push_back(5);
+	graph[5].push_back(6);
+	graph[6].push_back(7);
+	graph[7].push_back(8);
+
+	pair< vector<int>, vector<int> > pr = unweighted_shortest_path(graph, 0);
+	std::vector<int> distance = pr.first, path = pr.second;
+	ASSERT_EQ(distance.size(), path.size());
+	cout << "vertex | distance | path (prev node) " << endl;
+	for(int i = 0; i < distance.size(); i++){
+		cout << "   " << i << "        " << distance[i] << "        " << path[i] << endl;
+		EXPECT_EQ(i, distance[i]);
+		if(i != 0) EXPECT_EQ(i, path[i] + 1);
+	}
+}
+*/
