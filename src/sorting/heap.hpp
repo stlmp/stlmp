47cb9a45f4e27210248442ce14b118072584272c
Rishabh Bansal <rishabhbansal1855@gmail.com>
2017-09-28 11:49:14 +0530

    Add sorting implementations #13
    Added Heap Sort

---
+++ src/sorting/heap.hpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void heap_sort(int *a, int n)
+{
+    int i, temp;
+    for (i = n; i >= 2; i--)
+    {
+        temp = a[i];
+        a[i] = a[1];
+        a[1] = temp;
+        max_heapify(a, 1, i - 1);
+    }
+}
