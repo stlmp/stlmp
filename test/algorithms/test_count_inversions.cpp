1f9db75a7efdaa8550c7186ace55b77e3591d06d
Akash Goyal <aakash.g@hotmail.com>
2018-10-02 20:03:01 +0530

    Fixed test case for inversions

--- test/algorithms/test_count_inversions.cpp
+++ test/algorithms/test_count_inversions.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 TEST(count_inversions, all_tests){
-	  int arr[5] = {9, 3, 10, 11, 1};
+	int arr[] = {9, 3, 10, 11, 1};
     int arr_size = 5;
     count_inversions(arr,arr_size);
 
-    ASSERT_EQ(arr, arr_size);
+    ASSERT_EQ(arr[5], arr_size);
 }
